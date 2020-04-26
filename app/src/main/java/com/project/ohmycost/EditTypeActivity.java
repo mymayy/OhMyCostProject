77484c2283b37a993b5a39d794d3aac947fec662
= <amolvan27@gmail.com>
2020-04-26 15:40:20 +0700

    Edit extra

--- app/src/main/java/com/project/ohmycost/EditTypeActivity.java
+++ app/src/main/java/com/project/ohmycost/EditTypeActivity.java
@@ -1,14 +1,76 @@
 package com.project.ohmycost;
 
+//package com.project.ohmycostpay;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class EditTypeActivity extends AppCompatActivity {
+
+    private Button btnSave,btnDelete,btnBack;
+    private EditText editable_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_type);
+
+        btnSave = findViewById(R.id.btnSave);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnBack = findViewById(R.id.btnBack);
+        editable_item = findViewById(R.id.editable_item);
+
+        final ArrayList<String> typeEdit= (ArrayList<String>)getIntent().getExtras().getSerializable("type");
+        final String type=getIntent().getExtras().getString("typeEdit");
+        final String day = getIntent().getExtras().getString("day");
+        final String monthYear = getIntent().getExtras().getString("month");
+        final String Year = getIntent().getExtras().getString("year");
+        editable_item.setText(type);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = editable_item.getText().toString();
+                if(!item.equals("")){
+                    for(int i=0;i<typeEdit.size();i++){
+                        if(typeEdit.get(i).equals(type)){
+                            typeEdit.remove(i);
+                            typeEdit.add(i,item);
+                            toastMessage("Type Successfully Edit!");
+                        }
+                    }
+                }else{
+                    toastMessage("You must enter a type");
+                }
+            }
+        });
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                typeEdit.remove(type);
+                editable_item.setText("");
+                toastMessage("removed type");
+            }
+        });
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditTypeActivity.this, ListTypeActivity.class);
+                intent.putExtra("type",typeEdit);
+                intent.putExtra("day",day);
+                intent.putExtra("month",monthYear);
+                intent.putExtra("year",Year);
+                startActivity(intent);
+            }
+        });
+    }
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/project/ohmycost/ListTypeActivity.java
+++ app/src/main/java/com/project/ohmycost/ListTypeActivity.java
@@ -1,14 +1,63 @@
 package com.project.ohmycost;
 
-import androidx.appcompat.app.AppCompatActivity;
+//package com.project.ohmycostpay;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
 
 public class ListTypeActivity extends AppCompatActivity {
+
+    ListView mListView;
+    private Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_type);
+        mListView = findViewById(R.id.listView);
+        btnBack = findViewById(R.id.btnBack);
+
+        ArrayList<String> typeSelect= (ArrayList<String>)getIntent().getExtras().getSerializable("type");
+        final ArrayList<String> typeEdit = typeSelect;
+        final String day = getIntent().getExtras().getString("day");
+        final String monthYear = getIntent().getExtras().getString("month");
+        final String Year = getIntent().getExtras().getString("year");
+
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, typeEdit);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String type = adapterView.getItemAtPosition(i).toString();
+                Intent intent = new Intent(ListTypeActivity.this, EditTypeActivity.class);
+                intent.putExtra("typeEdit",type);
+                intent.putExtra("type",typeEdit);
+                intent.putExtra("day",day);
+                intent.putExtra("month",monthYear);
+                intent.putExtra("year",Year);
+                startActivity(intent);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListTypeActivity.this, TypeActivity.class);
+                intent.putExtra("type",typeEdit);
+                intent.putExtra("day",day);
+                intent.putExtra("month",monthYear);
+                intent.putExtra("year",Year);
+                startActivity(intent);
+            }
+        });
+
     }
 }
